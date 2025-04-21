ab97ff63e1e17d3d157477308b5e4aa53af49e59
infosys <infosys@C4202>
2025-04-21 09:57:58 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ProductSample/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+    //商品クラス
+    public class Product {
+        //商品コード
+        public int Code { get; private set; }                  //プロパティ
+        //商品名
+        public string Name { get; private set; }
+        //商品価格(税抜き)
+        public int Price { get; private set; }
+
+        //消費税率は10%
+        private readonly double _taxRate = 0.1;//フィールド
+
+        //消費税額を求める
+        public int GetTax() {　　　　　　　　　　　　//メゾッド
+            return (int)(Price * _taxRate);
+        }
+
+        //コンストラクター
+        public Product(int Code, String Name, int Price) {
+            this.Code = Code;
+            this.Name = Name;
+            this.Price = Price;
+        }
+
+        //消費込み価格を求める
+        public int GetPriceIncludingTax() {
+            return Price + GetTax();
+
+
+        }
+    }
+}
---
+++ Chapter01/ProductSample/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ProductSample {
+    internal class Program {
+        private static void Main(string[] args) {
+
+            Product karinto = new Product(123, "かりんとう", 180);
+
+
+            //税抜きの価格を表示（かりんとうの税抜き価格は○○円です）
+
+            Console.WriteLine(karinto.Name + "の税抜き価格は" + karinto.Price + "円です");
+
+            //消費税額の表示（かりんとうの消費税額は○○円です）
+
+            Console.WriteLine(karinto.Name + "の消費税額は" + karinto.GetTax + "です");
+
+            //税込み価格の表示（かりんとうの税込み価格は〇〇円です）
+
+            Console.WriteLine(karinto.Name + "の税込み価格は" +);
+
+
+        }
+    }
+}
