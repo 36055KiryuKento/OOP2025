f67fe37019369f79b99b54cd32a8312e57cf21cd
KiryuKento <oibc36055@ojs.ac.jp>
2025-04-22 10:00:33 +0900

    メッセージを追加

---
+++ Chapter02/DistanceConverter/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DistanceConverter {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+
+           
+        }
+    }
+}
