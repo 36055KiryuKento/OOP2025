686570cd56b2d24811ee5999de9eb2f4ff1f999a
KiryuKento <oibc36055@ojs.ac.jp>
2025-04-22 10:26:08 +0900

    インスタンスメソッドとして利用

--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -1,9 +1,33 @@
 ﻿namespace DistanceConverter {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
 
-           
+            int start = int.Parse(args[1]);
+            int end = int.Parse(args[2]);
+
+            if (args.Length >= 1 && args[0] == "-tom") {
+                PrintFeetToMeterList(start, end);
+            } else {
+                PrintMeterToFeetList(start, end);
+            }
+        }
+        //フィートからメートルへの対応表を出力
+        static void PrintFeetToMeterList(int start, int end) {
+            FeetConverter converter = new FeetConverter();
+            for (int feet = start; feet <= end; feet++) {
+                double meter = converter.ToMeter(feet);
+                Console.WriteLine($"{feet}ft = {meter:0.0000}m");
+            }
+        }
+
+        //メートルからフィートへの対応表を出力
+        static void PrintMeterToFeetList(int start, int end) {
+            FeetConverter converter = new FeetConverter();
+            for (int meter = start; meter <= end; meter++) {
+                double feet = converter.FromMeter(meter);
+                Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+
+            }
         }
     }
 }
