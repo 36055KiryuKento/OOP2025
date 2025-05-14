49a5dc0172305c15421da044576bc9d76315ea39
KiryuKento <oibc36055@ojs.ac.jp>
2025-05-14 11:17:37 +0900

    0514演習
    ４５のエラーが自力で取れませんでした

--- Chapter02/演習問題/Exercise01/Song.cs
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -7,17 +7,17 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     //2.1.1
     public class Song {
-        public string Title { get; private set; }
-        public string ArtistName { get; private set; }
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
         public int Length { get; set; }
 
 
         //2.1.2
-        public Song(String Title, String ArtistName, int Length) {
-            this.Title = Title;
-            this.ArtistName = ArtistName;
-            this.Length = Length;
-        }
+       // public Song(String Title, String ArtistName, int Length) {
+           // Title = title;
+            //ArtistName = artistname;
+            //Length = length;
+       // }
 
     }
 
