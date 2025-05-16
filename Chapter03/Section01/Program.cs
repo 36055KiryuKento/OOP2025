3ced1ea29f2d0a3961b96eb706e1dbb3b349d47d
KiryuKento <oibc36055@ojs.ac.jp>
2025-05-16 14:56:55 +0900

    P71まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -3,27 +3,22 @@
 
 
         static void Main(string[] args) {
-           // Console.WriteLine("カウントしたい数値：");
-            //int num = int.Parse(Console.ReadLine());
+            var cities = new List<string> {
+    "Tokyo",
+    "New Delhi",
+    "Bangkok",
+    "London",
+    "Paris",
+    "Berlin",
+    "Canberra",
+    "Hong Kong",
+};
 
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var loewerList = cities.ConvertAll(s => s.ToUpper());
+            loewerList.ForEach(s => Console.WriteLine(s));
 
-            Console.WriteLine(Count(numbers, n=> 5 <= n && n< 10 ));
 
+             }
         }
-
-        static int Count(int[] numbers, Predicate<int> judge) {
-            var count = 0;
-            foreach(var n in numbers) {
-                //引数で受け取ったメソッドを呼び出す
-                if(judge(n) == true) {
-                    count++;
-                   
-                }
-               
-            }
-            return count;
-        }
-
     }
-}
+
