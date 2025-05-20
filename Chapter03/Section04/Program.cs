9221c5a4344e9ac7a1e624a5bf1324b7be765506
KiryuKento <oibc36055@ojs.ac.jp>
2025-05-20 11:18:33 +0900

    3.2.2完成

--- Chapter03/Section04/Program.cs
+++ Chapter03/Section04/Program.cs
@@ -1,7 +1,37 @@
 ﻿namespace Section04 {
     internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            static void Main(string[] args) {
+                var cities = new List<string> {
+                "Tokyo", "New Delhi", "Bangkok", "London",
+                "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+
+            var query = cities.Where(s => s.Length <= 5).ToArray();    //即時実行
+
+
+
+
+            foreach (var item in query) {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------");
+
+            cities[0] = "Osaka";            //- cities[0]を変更 
+            foreach (var item in query) {   //- 再度、queryの内容を取り出す[遅延実行]
+                Console.WriteLine(item);
+            }
+
+            //IEnumerable<string> query = cities.Reverse(s=>);
+            //.Where(s => s.Length <= 5)//条件にあったものを抽出
+            //.Select(s => s.ToLower());//別オブジェクトへ変換（新しい型へ射影する）
+            // .OrderBy(s => s);
+
+
+
+           // foreach (var s in query) {
+                    //Console.WriteLine(s);
+
+                }
+            }
         }
-    }
-}
