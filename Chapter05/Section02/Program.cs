9c5b2b8893e53a6fb2efd82deeb40d65016a6729
KiryuKento <oibc36055@ojs.ac.jp>
2025-06-06 15:59:53 +0900

    演習問題

---
+++ Chapter05/Section02/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var appVer1 = new AppVersion(5, 1);
+            var appVer2 = new AppVersion(5, 1);
+
+            Console.WriteLine(appVer1);
+           // if(appVer1 == appVer2) {
+             //   Console.WriteLine("等しい");
+            //} else {
+           //     Console.WriteLine("等しくない");
+           // }
+        }
+    }
+
+    //プライマリーコンストラクタを使ったクラス定義
+  public class AppVersion(int Major, int Minor, int Build = 0, int Revision = 0);
+
+         
+        }
+
---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //5.1.1
+    public class YearMonth {
+        public int Year { get;  }
+
+        public int Month { get;  }
+
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+       
+
+    //5.1.2
+    //設定されている西暦が２１世紀か判定する
+    //2001～2100年の間ならtrue、それ以外ならfalseを返す
+    public bool Is21Century =>   Year >= 2001 && Year <= 2100;
+
+    //5.1.3
+    public YearMonth AddOneMonth() {
+            if (Month<=12) {
+                return new YearMonth( Year,Month+1 );//12月以外
+            } else {
+                return new YearMonth(Year+1 ,Month-11 );//12月
+            }
+        }
+
+        //5.1.4
+        public override string ToString() =>
+           }
+}
