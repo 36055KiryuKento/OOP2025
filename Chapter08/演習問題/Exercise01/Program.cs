70419acb3832d1c2c6ecbdd92758137738529107
KiryuKento <oibc36055@ojs.ac.jp>
2025-06-13 13:39:02 +0900

    演習問題8.1.2

--- Chapter08/演習問題/Exercise01/Program.cs
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Security.Cryptography.X509Certificates;
+
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
@@ -10,31 +12,54 @@ namespace Exercise01 {
             Exercise2(text);
 
         }
-       
 
-private static void Exercise1(string text) {
-    var dict = new Dictionary<Char, int>();
 
-   
-        for (var ch = 'A'; ch <= 'Z'; ch++) {
-            dict[ch] = 0;
+        private static void Exercise1(string text) {
+            var dict = new Dictionary<Char, int>();
 
-        }
+            // A〜Z をキーとして初期化
+            for (var ch = 'A'; ch <= 'Z'; ch++) {
+                dict[ch] = 0;
+
+            }
+            // テキストを大文字にしてループ
             foreach (var c in text.ToUpper()) {
                 if ('A' <= c && c <= 'Z') {
                     dict[c]++;
 
                 }
             }
-
-                foreach(var s in dict) {
-                    Console.WriteLine($"{s.Key}:{s.Value}");
-                }
-
+            // 並び変えて結果を出力
+            foreach (var s in dict) {
+                Console.WriteLine($"{s.Key}:{s.Value}");
             }
 
-private static void Exercise2(string text) {
+        }
 
-}
+        private static void Exercise2(string text) {
+            var dict = new SortedDictionary<Char, int>();
+
+            // A〜Z をキーとして初期化
+            for (var ch = 'A'; ch <= 'Z'; ch++) {
+                dict[ch] = 0;
+
+            }
+            // テキストを大文字にしてループ
+            foreach (var c in text.ToUpper()) {
+                if ('A' <= c && c <= 'Z') {
+                    dict[c]++;
+
+                }
+            }
+            // 結果を出力
+            foreach (var s in dict) {
+                Console.WriteLine($"{s.Key}:{s.Value}");
+            }
+
+        }
     }
-}
+    }
+
+
+
+
