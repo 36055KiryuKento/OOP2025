d746f352b96fedd9e3e69b2423db2857dabcb706
KiryuKento <oibc36055@ojs.ac.jp>
2025-06-17 10:33:23 +0900

    8演習問題　完成

--- Chapter08/演習問題/Exercise02/Program.cs
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Exercise02 {
+﻿using System.Net.NetworkInformation;
+
+namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
             // コンストラクタの呼び出し
@@ -7,6 +9,28 @@
             // Addメソッドの呼び出し例
             abbrs.Add("IOC", "国際オリンピック委員会");
             abbrs.Add("NPT", "核兵器不拡散条約");
+
+            //8.2.3(Countの呼び出し例）
+            // 上のAddメゾットで、２つのオブジェクトを追加しているので、読み込んだ単語数+2が、Countの値になる。
+            var count = abbrs.Count;
+            Console.WriteLine(abbrs.Count);
+            Console.WriteLine();
+
+            //8.2.3(Removeの呼び出し例)
+            if (abbrs.Remove("NPT")) {
+                Console.WriteLine(abbrs.Count);
+            }
+
+            //すでに消去してあるので、falseが返る
+            if (!abbrs.Remove("NPT")) {
+                Console.WriteLine("消去できません");
+            }
+            Console.WriteLine();
+
+            //8.2.4
+            var query = abbrs.GetAll().Where(x => x.Key.Length == 3);
+
+
 
             // Getメソッドの利用例
             var names = new[] { "WHO", "FIFA", "NPT", };
@@ -35,6 +59,8 @@
                 Console.WriteLine($"{key}={value}");
             }
             Console.WriteLine();
+
+
         }
     }
 }
