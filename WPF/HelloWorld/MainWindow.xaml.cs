5302e076e792bbbb0dca6a8bfe5b811af992e470
KiryuKento <oibc36055@ojs.ac.jp>
2025-10-02 14:51:00 +0900

    変更

--- WPF/HelloWorld/MainWindow.xaml.cs
+++ WPF/HelloWorld/MainWindow.xaml.cs
@@ -9,15 +9,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HelloWorld;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window{
-    public MainWindow()
-    {
-        InitializeComponent();
-        //DataContext = new ViewModel();
+namespace HelloWorld {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            //DataContext = new ViewModel();
+        }
     }
 }
--- WPF/HelloWorld/ViewModel.cs
+++ WPF/HelloWorld/ViewModel.cs
@@ -12,21 +12,20 @@ namespace HelloWorld
         public ViewModel() {
             ChangeMessageCommand = new DelegateCommand(
                 () => GreetingMessage = "Bye-bye world");
-
         }
 
-
-        private string _greetingMessage = "Hello World";
-        public string GreetingMessage {
-            get => _greetingMessage;
+        private string _greetingMessage = "Hello World!";
+        public string GreetingMessage { 
+            get=>_greetingMessage;
             set {
-                if (_greetingMessage != value) {
+                if(_greetingMessage != value) {
                     _greetingMessage = value;
                     PropertyChanged?.Invoke(
                         this, new PropertyChangedEventArgs(nameof(GreetingMessage)));
                 }
             }
         }
+
         public DelegateCommand ChangeMessageCommand { get; }
 
         public event PropertyChangedEventHandler? PropertyChanged;
