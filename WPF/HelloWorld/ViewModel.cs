319d0ce32c35b832c815603438f961facc7147b3
KiryuKento <oibc36055@ojs.ac.jp>
2025-10-06 15:28:18 +0900

    途中

--- WPF/HelloWorld/ViewModel.cs
+++ WPF/HelloWorld/ViewModel.cs
@@ -12,25 +12,16 @@ namespace HelloWorld
     {
         public ViewModel() {
             ChangeMessageCommand = new DelegateCommand<string>(
-              (par) => GreetingMessage = par);
+              (par) => GreetingMessage = par,
+              (par)=>GreetingMessage !=par)
+               .ObservesProperty(() => GreetingMessage);
         }
 
         private string _greetingMessage = "Hello World!";
         public string GreetingMessage { 
             get=>_greetingMessage;
-            set {
-                    if(SetProperty(ref _greetingMessage, value)) {
-                    CanChangeMessage = false;
-                }
-
+            set=> SetProperty(ref _greetingMessage, value);
                 
-            }
-        }
-
-        private bool _canChangeMessage = true;
-        public bool CanChangeMessage {
-            get => _canChangeMessage;
-            private set => SetProperty(ref _canChangeMessage, value);
         }
 
         public string NewMessage1 { get; } = "Bye-bye world";
---
+++ WPF/PrismObservesSample/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismObservesSample{
+    class MainWindowViewModel : BindableBase{
+        private string _input1 = "";
+        public string Input1 {
+            get => _input1;
+            set => SetProperty( ref _input1, value);
+        }
+
+        private string _input2 = "";
+        public string Input2 {
+            get => _input2;
+            set => SetProperty(ref _input2, value);  
+        }
+
+        private string _result = "";
+        public string Result {
+            get => _result;
+            set => SetProperty(ref _result, value);
+        }
+        public DelegateCommand SumCommand { get; }
+        //コンストラクタ
+        public MainWindowViewModel() {
+            SumCommand = new DelegateCommand(ExcuteSum);
+        }
+       
+
+
+        //足し算の処理
+        private void ExcuteSum() {
+    
+                Result = (Input1 + Input2).ToString();
+           
+        }
+        //足し算処理を実行できるか否かのチェック
+        private bool canExcuteSum() {
+
+        }
+    }
+}
