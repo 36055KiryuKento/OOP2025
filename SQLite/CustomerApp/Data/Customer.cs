58ea55b9e8dce8dfc159225559eaae376b113c3a
KiryuKento <oibc36055@ojs.ac.jp>
2025-10-27 14:50:48 +0900

    Revert "kadai2"
    This reverts commit e0c7dce73620cc13ed038ab5783b2f11536b7912.

--- SQLite/CustomerApp/Data/Customer.cs
+++ SQLite/CustomerApp/Data/Customer.cs
@@ -1,19 +1,17 @@
 ﻿using SQLite;
-using System.IO;
-using System.Windows.Media.Imaging;
 
 namespace CustomerApp.Data {
-    class Customer {
-        [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
-        /// <summary>
-        /// 名前
-        /// </summary>
-        public string Name { get; set; } = string.Empty;
-        /// <summary>
-        /// 電話番号
-        /// </summary>
-        public string Phone { get; set; } = string.Empty;
+        class Customer {
+            [PrimaryKey, AutoIncrement]
+            public int Id { get; set; }
+            /// <summary>
+            /// 名前
+            /// </summary>
+            public string Name { get; set; } = string.Empty;
+            /// <summary>
+            /// 電話番号
+            /// </summary>
+            public string Phone { get; set; } = string.Empty;
         /// <summary>
         ///  住所
         /// </summary>
@@ -22,24 +20,5 @@ namespace CustomerApp.Data {
         /// 写真
         /// </summary>
         public byte[] Picture { get; set; }
-
-        // 画像をListViewで表示するためのプロパティ
-        [Ignore]  // SQLiteに保存しない
-        public BitmapImage PhotoImage {
-            get {
-                if (Picture == null || Picture.Length == 0)
-                    return null;
-
-                using (var ms = new MemoryStream(Picture)) {
-                    var bmp = new BitmapImage();
-                    bmp.BeginInit();
-                    bmp.CacheOption = BitmapCacheOption.OnLoad;
-                    bmp.StreamSource = ms;
-                    bmp.EndInit();
-                    bmp.Freeze(); // メモリリーク防止
-                    return bmp;
-                }
-            }
-        }
     }
 }
--- SQLite/CustomerApp/MainWindow.xaml.cs
+++ SQLite/CustomerApp/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace CustomerApp {
     /// </summary>
     public partial class MainWindow : Window {
 
+        OpenFileDialog ofd;
         private List<Customer> _customer = new List<Customer>();
 
         public MainWindow() {
@@ -62,7 +63,7 @@ namespace CustomerApp {
         private void UpdateButton_Click(object sender, RoutedEventArgs e) {
 
             using (var connection = new SQLiteConnection(App.databasePath)) {
-            
+                connection.CreateTable<Customer>();
 
                 var selectedPerson = PersonListView.SelectedItem as Customer;
                 if (selectedPerson is null) return;
@@ -72,7 +73,7 @@ namespace CustomerApp {
                     Name = NameTextBox.Text,
                     Phone = PhoneTextBox.Text,
                     Address = addressTextBox.Text,
-                    Picture = ImageSourceToByteArray(SelectedImage.Source)
+
                 };
 
                 connection.Update(person);
@@ -94,7 +95,7 @@ namespace CustomerApp {
             };
 
             using (var connection = new SQLiteConnection(App.databasePath)) {
-               
+                connection.CreateTable<Customer>();
                 connection.Insert(person);
             }
             ReadDatabase(); // 保存後リスト更新
@@ -112,8 +113,10 @@ namespace CustomerApp {
 
             //データベース接続
             using (var connection = new SQLiteConnection(App.databasePath)) {
+                connection.CreateTable<Customer>();
                 connection.Delete(item);  //データベースから選択されているレコードの削除
                 ReadDatabase();
+                PersonListView.ItemsSource = _customer;
             }
         }
 
@@ -168,6 +171,20 @@ namespace CustomerApp {
                 byteArray = stream.ToArray();
             }
             return byteArray;
+        }
+
+        public static BitmapImage byteToBitmap(byte[] bytes) {
+            var result = new BitmapImage();
+
+            using (var stream = new MemoryStream(bytes)) {
+                result.BeginInit();
+                result.CacheOption = BitmapCacheOption.OnLoad;
+                result.CreateOptions = BitmapCreateOptions.None;
+                result.StreamSource = stream;
+                result.EndInit();
+                result.Freeze();    // 非UIスレッドから作成する場合、Freezeしないとメモリリークするため注意
+            }
+            return result;
         }
     }
 }
