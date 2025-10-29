67d2105ade7cf7e1ff609c8ba1e9e82d422cc51c
KiryuKento <oibc36055@ojs.ac.jp>
2025-10-29 10:38:15 +0900

    教科書

---
+++ Chapter13/Exercise01/Program.cs
@@ -0,0 +1,69 @@
+﻿
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_2();
+            Console.WriteLine();
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+        }
+
+        private static void Exercise1_2() {
+            var book = Library.Books.MaxBy(b => b.Price);
+            Console.WriteLine(book);
+            
+           
+           
+        }
+
+
+        private static void Exercise1_3() {
+            var results = Library.Books
+                .GroupBy(b => b.PublishedYear)
+                .OrderBy(b => b.Key)
+                .Select(b => new {
+                    PublishedYear = b.Key,
+                    Count = b.Count(),
+             });
+        }
+        
+
+        private static void Exercise1_4() {
+            var books = Library.Books
+                .OrderDescending(x => x.PublishedYear)
+                .Then
+
+                 foreach (var book in books) {
+                Console.WriteLine($"{item.PublishedYear}年 {item.Title} {item.Price}");
+            }
+        }
+
+        private static void Exercise1_5() {
+
+        }
+
+        private static void Exercise1_6() {
+
+        }
+
+        private static void Exercise1_7() {
+
+        }
+
+        private static void Exercise1_8() {
+
+        }
+    }
+}
