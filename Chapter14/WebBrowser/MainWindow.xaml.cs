71a2e097c9f4671c88d87aa85e87a990b809eec4
KiryuKento <oibc36055@ojs.ac.jp>
2025-11-06 14:48:09 +0900

    完成

--- Chapter14/WebBrowser/MainWindow.xaml.cs
+++ Chapter14/WebBrowser/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.Net.Http;
+﻿using Microsoft.Web.WebView2.Core;
+using System.Net.Http;
 using System.Reflection.Emit;
 using System.Security.Policy;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,25 +22,44 @@ namespace WebBrowser {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            InitializeAsync();
+        }
+        private async void InitializeAsync() {
+            await WebView.EnsureCoreWebView2Async();
+
+            WebView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
+            WebView.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationCompleted;
+        }
+        //読み込み開始したらプログレスバーを表示
+        private void CoreWebView2_NavigationStarting(object? sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e) {
+            LoadingBar.Visibility = Visibility.Visible;
+            LoadingBar.IsIndeterminate = true;
+        }
+        //読み込みが完了したらプログレスバーを非表示
+        private void CoreWebView2_NavigationCompleted(object? sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e) {
+            LoadingBar.Visibility = Visibility.Collapsed;
+            LoadingBar.IsIndeterminate = false;
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e) {
-
+            if (WebView.CanGoBack) {
+                WebView.GoBack();
+            }
         }
 
         private void FowardButton_Click(object sender, RoutedEventArgs e) {
-            label1.Visible = false;
-            if (wvRssLink.CanGoForward) {
-                wvRssLink.GoForward();
-            } else {
-                lblStatus.Text = "これ以上進めません。";
-                lblStatus.Visible = true;   // メッセージ表示
+            if (WebView.CanGoForward) {
+                WebView.GoForward();
             }
         }
-        
+
 
         private void GoButton_Click(object sender, RoutedEventArgs e) {
+            var url = AddressBar.Text.Trim();
 
+            if (string.IsNullOrWhiteSpace(url)) return;
+
+            WebView.Source = new Uri(url);
         }
     }
 }
