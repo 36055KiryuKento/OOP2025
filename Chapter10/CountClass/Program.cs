0304f4ab62f0058ed1d9c21e612c5c3d3d895a38
KiryuKento <oibc36055@ojs.ac.jp>
2025-11-12 10:14:53 +0900

    ポリモーふぃずむ　

---
+++ Chapter10/CountClass/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace CountClass {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Chapter15/Section01/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<GreetingBase> list = [
+                new GreetingMorning(),
+               new GreetingAfternoon(),
+               new GreetingEvening(),
+               ];
+
+            foreach (var obj in list) {
+                string msg = obj.GetMessarge();
+                Console.WriteLine(msg);
+            }
+        }
+
+        class GreetingMorning : GreetingBase {
+            public override string GetMessarge() => "おはよう";
+        }
+
+        class GreetingAfternoon : GreetingBase {
+            public override string GetMessarge() => "こんにちわ";
+        }
+
+        class GreetingEvening : GreetingBase {
+            public override string GetMessarge() => "こんばんわ";
+        }
+    }
+}
