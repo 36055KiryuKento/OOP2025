498108d2bca7bc86c498feb2763cade671ad051c
KiryuKento <oibc36055@ojs.ac.jp>
2025-11-12 09:33:15 +0900

    section05

--- Chapter14/Section05/Program.cs
+++ Chapter14/Section05/Program.cs
@@ -1,18 +1,40 @@
 ﻿using Section01;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Section05 {
     internal class Program {
-        static void Main(string[] args) {
-            var selected = Library.Books
-                .AsParallel()
-                .AsOrdered()
-                .Where(b => b.Price > 500 && b.Price < 2000)
-                .Select(b => new { b.Title });
+        static async Task Main(string[] args) {
 
-            foreach (var item in selected) {
-                Console.WriteLine(item.Title);
-            }
+            var ta = new TaskExample();
+            await ta.Run();
+
+
         }
     }
 
+
+
+    // Code 14.22
+    public class TaskExample {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task Run() {
+            var tasks = new Task<string>[] {
+            GetPageAsync(@"https://business.nikkei.com/rss/sns/nb.rdf"),
+            GetPageAsync(@"https://www.nhk.or.jp/rss/news/cat3.xml"),
+        };
+            var results = await Task.WhenAll(tasks);
+
+            // それぞれ先頭400文字を表示する
+            var text = $"{results[0].Substring(0, 400)}\n\n{results[1].Substring(0, 400)}";
+            Console.WriteLine(text);
+        }
+
+
+        private async Task<string> GetPageAsync(string urlstr) {
+            var str = await _httpClient.GetStringAsync(urlstr);
+            return str;
+        }
+    }
 }
